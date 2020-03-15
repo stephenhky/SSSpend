3a3eb8738ba39af43f2653de1f5860fca20a47d2
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-03-14 20:02:51 -0400

    methods for extracting data frame

--- ssspend/spenddataframe.py
+++ ssspend/spenddataframe.py
@@ -1,3 +1,15 @@
 
 import pandas as pd
+from gsheets import Sheets
 
+def get_google_spreadsheet(my_client_secret, my_client_storage, url):
+    sheets = Sheets.from_files(my_client_secret, my_client_storage)
+    spreadsheet = sheets.get(url)
+    return spreadsheet
+
+
+def get_month_dataframe(spreadsheet, month):
+    df = spreadsheet.find(month).to_frame(index_col='Unnamed: 0')
+    df = df[['Date', 'Place', 'Category', 'City', 'Debit', 'Comment', 'Individual',
+             'Payment Method']]
+    return df
