cccdb0b59a93bffae3066f0ca843d39d88e0e5de
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-02-03 13:45:00 -0500

    new return

--- awsecsimage/ssspend_summary_handler.py
+++ awsecsimage/ssspend_summary_handler.py
@@ -1,19 +1,16 @@
 
-import argparse
 import json
 import os
 import logging
 
 import boto3
 
-from ssspend.spenddataframe import get_google_spreadsheet_from_oauth2, \
-    get_google_spreadsheet_from_apikey, get_google_spreadsheet_from_service_account
+from ssspend.spenddataframe import get_google_spreadsheet_from_service_account
 from ssspend.summarydataframe import generate_summary_dict, generate_summary_df, \
     update_summary_googlespreadsheet
 
-argparser = argparse.ArgumentParser(description='Update SSSpend summary')
-argparser.add_argument('url', help='URL of spreadsheet')
-argparser.add_argument('--serviceaccount', default=None, type=str, help='path of the service account info (*.json)')
+
+service_account_filename = 'service_account.json'
 
 
 def lambda_handler(event, context):
@@ -25,23 +22,34 @@ def lambda_handler(event, context):
     logging.info('spreadsheet URL: {}'.format(url))
 
     # getting environment variables
-    s3_bucket_name = os.environ.get('s3_bucket')
-    service_account_file = os.environ.get('service_account_file')
+    s3_bucket_name = os.environ.get('S3_BUCKET')
+    service_account_file = os.environ.get('SERVICE_ACCOUNT_FILE')
 
     # get service account info
     s3_client = boto3.client('s3')
-    s3_client.download_file(s3_bucket_name, service_account_file, 'service_account.json')
-    service_account_info = json.load(open('service_account.json', 'r'))
+    s3_client.download_file(
+        s3_bucket_name,
+        service_account_file,
+        os.path.join('/', 'tmp', service_account_filename)
+    )
+    service_account_info = json.load(open(os.path.join('/', 'tmp', service_account_filename), 'r'))
 
     # handling Google spreadsheet
     spreadsheet = get_google_spreadsheet_from_service_account(service_account_info, url)
     monthly_summary_dict = generate_summary_dict(spreadsheet)
     summary_df = generate_summary_df(monthly_summary_dict)
-    update_summary_googlespreadsheet(url, summary_df, service_account_file='service_account.json')
+    update_summary_googlespreadsheet(
+        url,
+        summary_df,
+        service_account_file=os.path.join('/', 'tmp', service_account_filename)
+    )
 
     return {
         'isBase64Encoded': False,
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': json.dumps({
+            'url': url,
+            'summary': monthly_summary_dict
+        })
     }
 
